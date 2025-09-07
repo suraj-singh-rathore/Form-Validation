b31eef009000a6307c29673d50c8413aaa429128
suraj-singh-rathore <ssingh70115@gmail.com>
2025-09-07 23:39:27 +0530

    Update Changes

---
+++ script.js
@@ -0,0 +1,22 @@
+const submitBtn = document.getElementById('submitBtn');
+const nameError = document.getElementById('nameError');
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (validateName()) {
+        alert("Form Submitted Successfully");
+    }
+});
+
+function validateName() {
+    let name = document.getElementById('name').value;
+
+    if (name.length == 0) {
+        nameError.innerHTML = "Name is Required";
+        return false;
+    } else {
+        nameError.innerHTML = ""; 
+        return true;
+    }
+}
