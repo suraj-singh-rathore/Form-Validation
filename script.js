89c357b0692f508e823c80c410f4dc0ab937ed65
suraj-singh-rathore <ssingh70115@gmail.com>
2025-09-08 22:25:09 +0530

    Added validation through js

--- script.js
+++ script.js
@@ -1,22 +1,75 @@
 const submitBtn = document.getElementById('submitBtn');
 const nameError = document.getElementById('nameError');
+const emailError = document.getElementById('emailError');
+const passwordError = document.getElementById('passwordError');
 
-submitBtn.addEventListener('click', (e) => {
+submitBtn.addEventListener('click', (e)=>{
     e.preventDefault();
 
-    if (validateName()) {
-        alert("Form Submitted Successfully");
+    if(validateName() & validateEmail() & validatePassword()){
+        alert("Form submitted Successfully");
     }
 });
 
-function validateName() {
+//Name validation:
+
+function validateName(){
     let name = document.getElementById('name').value;
 
-    if (name.length == 0) {
-        nameError.innerHTML = "Name is Required";
+    if(name.length == 0){
+        nameError.innerHTML = "Name is required*";
+        nameError.previousElementSibling.classList.add('fa-xmark');
         return false;
-    } else {
-        nameError.innerHTML = ""; 
-        return true;
     }
+
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = "Write full Name";
+        nameError.previousElementSibling.classList.add('fa-xmark');
+        return false;
+    }
+    nameError.innerHTML = "";
+    nameError.previousElementSibling.classList.add('fa-check');
+    return true;
+}
+
+//Email:
+
+function validateEmail(){
+    let email = document.getElementById('email').value;
+
+    if(email.length == 0){
+        emailError.innerHTML = "Email is required*";
+        emailError.previousElementSibling.classList.add('fa-xmark');
+        return false;
+    }
+
+    if(!email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)){
+        emailError.innerHTML = "Enter valid email";
+        emailError.previousElementSibling.classList.add('fa-xmark');
+        return false;
+    }
+    emailError.innerHTML = "";
+    emailError.previousElementSibling.classList.add('fa-check');
+    return true;
+}
+
+//Password:
+
+function validatePassword(){
+    let password = document.getElementById('password').value;
+
+    if(password.length == 0){
+    passwordError.innerHTML = "Password is required*";
+    passwordError.previousElementSibling.classList.add('fa-xmark');
+        return false;
+    }
+
+    if(!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)){
+        passwordError.innerHTML = "Password should contain 1 Uppercase, 1 Lowercase, 1 Digit & 1 Alphabet";
+        passwordError.previousElementSibling.classList.add('fa-xmark');
+        return false;
+    }
+    passwordError.innerHTML = "";
+    passwordError.previousElementSibling.classList.add('fa-check');
+    return true;
 }
